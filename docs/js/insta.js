b03d0d5041b6d41a9f8e248c59c0fd4da494a433
darcons <157471835+darcons@users.noreply.github.com>
2024-10-05 23:14:48 +0300

    Седьмой коммит
    попытка исправления ошибок предыдущих коммитов

--- docs/js/insta.js
+++ docs/js/insta.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   var feed = new Instafeed({
-    // get: "user",
-    // userID: 512819861478832,
+    get: "user",
+    userID: 512819861478832,
     accessToken:
-      "IGQWRQRWJHd29hY1lBcENaS1E2UVJuRXdEY0NENGhNS09pSzZAoSEgxQUJyWTBvZAkFxV0pNdTROakd4QzZA0UFlFdTJpLVpxNUFaSGN2a3htV21oUER3SUgwdUx3bkNEOWkxQmJKQnRydlJLMDA2RzJmOFB2OXNZARFkZD",
+      "IGQWRQZAEZAYTFplUXRiTjlNcEZAZAQWp4Y3I4bEtkNk5NWGtZAN1FxSnZAidFFJbFNXakJxVk5HMEpKdk1idFhVVHpNNFJXWVozSmJxYnlodHFHeWt0SDJsczJSeElzODBDMXhtUHc5WWRHNDdnZA2N1dHk3UnpLQjFJZAFkZD",
     resolution: "standart_resolution",
   });
   feed.run();
