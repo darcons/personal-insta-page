ceb5d59ececae0a838b89dc680311f8a09f54157
darcons <157471835+darcons@users.noreply.github.com>
2024-10-05 22:30:22 +0300

    Update insta.js

--- docs/js/insta.js
+++ docs/js/insta.js
@@ -5,10 +5,9 @@ $(document).ready(function () {
     accessToken:
       "IGQWRPOXA2Vk5vOXlOSU81ZATFfTV9mN1A0OXgzcjNrMFZAVQkVEMHNoNkZAmWnBmTVh5OXJSN3hMVHBfRldEVnVPSFRGR0pGUEVpdVFKX2cxa1luWVFkbUNqcGM4bVdTT19ibjJjbGFRQTlHZAmpxV0ZA2c1FqTjZAKVEUZD",
     resolution: "standart_resolution",
-
   });
   feed.run();
-  setTimeout(function() {$(".instafeed").addClass("shadowed");
-    
+  setTimeout(function () {
+    $(".instafeed").addClass("shadowed");
   }, 800);
 });
