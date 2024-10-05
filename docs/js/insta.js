49c172b5b87e935d3a9b244750b4dd1616c326ad
darcons <157471835+darcons@users.noreply.github.com>
2024-10-05 22:27:04 +0300

    Четвертый коммит
    добавление доступа к инстаграм
    изменение в файлах: photo.css, index.html, insta.js, slideshow.js

--- docs/js/insta.js
+++ docs/js/insta.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){ 
+$(document).ready(function () {
+  var feed = new Instafeed({
+    get: "user",
+    userID: 512819861478832,
+    accessToken:
+      "IGQWRPOXA2Vk5vOXlOSU81ZATFfTV9mN1A0OXgzcjNrMFZAVQkVEMHNoNkZAmWnBmTVh5OXJSN3hMVHBfRldEVnVPSFRGR0pGUEVpdVFKX2cxa1luWVFkbUNqcGM4bVdTT19ibjJjbGFRQTlHZAmpxV0ZA2c1FqTjZAKVEUZD",
+    resolution: "standart_resolution",
 
-
-
+  });
+  feed.run();
+  setTimeout(function() {$(".instafeed").addClass("shadowed");
+    
+  }, 800);
 });
