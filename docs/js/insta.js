b0a1a71a44cac34300cbb08b7695b99843e18193
darcons <157471835+darcons@users.noreply.github.com>
2024-10-13 08:32:55 +0300

    Восьмой комит
    Удаления кода доступа в инстаграм

--- docs/js/insta.js
+++ docs/js/insta.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
   var feed = new Instafeed({
     get: "user",
-    userID: 512819861478832,
-    accessToken:
-      "IGQWRQZAEZAYTFplUXRiTjlNcEZAZAQWp4Y3I4bEtkNk5NWGtZAN1FxSnZAidFFJbFNXakJxVk5HMEpKdk1idFhVVHpNNFJXWVozSmJxYnlodHFHeWt0SDJsczJSeElzODBDMXhtUHc5WWRHNDdnZA2N1dHk3UnpLQjFJZAFkZD",
+
     resolution: "standart_resolution",
   });
   feed.run();
